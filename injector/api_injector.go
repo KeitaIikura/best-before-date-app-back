550201f07aedd2e395dace628b3ccd77077dcb92
KeitaIikura <k.iikura0620@gmail.com>
2022-12-02 16:58:49 +0900

    雛形修正

--- injector/api_injector.go
+++ injector/api_injector.go
@@ -2,6 +2,9 @@ package injector
 
 import (
 	controller "bbdate/internal/bbdate/adapter/controller"
+	"bbdate/internal/bbdate/adapter/gateway"
+	bbdate_usecase "bbdate/internal/bbdate/application/usecase"
+	"bbdate/internal/bbdate/domain/service"
 	"bbdate/pkg/logging"
 
 	"go.uber.org/multierr"
@@ -13,9 +16,17 @@ func provideAPIInjection() {
 	err = multierr.Append(err, c.Provide(controller.NewAuthController))
 
 	// usecase
-	// err = multierr.Append(err, c.Provide(management_usecase.NewAuthLoginInteractor))
+	err = multierr.Append(err, c.Provide(bbdate_usecase.NewAuthCheckInteractor))
+	err = multierr.Append(err, c.Provide(bbdate_usecase.NewAuthLoginInteractor))
+	err = multierr.Append(err, c.Provide(bbdate_usecase.NewAuthLogoutInteractor))
+	err = multierr.Append(err, c.Provide(bbdate_usecase.NewAuthUpdatePasswordInteractor))
 
 	// service
+	err = multierr.Append(err, c.Provide(service.NewAuthService))
+
+	// repository (gateway)
+	err = multierr.Append(err, c.Provide(gateway.NewAuthUserGateway))
+
 	if err != nil {
 		errs := multierr.Errors(err)
 		for _, openErr := range errs {
